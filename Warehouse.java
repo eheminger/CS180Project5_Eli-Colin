085e0298ac21c8011355732915804117ce4fd9ce
eheminger <42813307+eheminger@users.noreply.github.com>
2018-12-04 18:57:53 -0500

    Updated Warehouse

--- Warehouse.java
+++ Warehouse.java
@@ -8,30 +8,33 @@ import java.util.Scanner;
  */
 
 public class Warehouse {
-    final static String folderPath = "files/";
+	final static String folderPath = "files/";
     final static File VEHICLE_FILE = new File(folderPath + "VehicleList.csv");
     final static File PACKAGE_FILE = new File(folderPath + "PackageList.csv");
     final static File PROFIT_FILE = new File(folderPath + "Profit.txt");
     final static File N_PACKAGES_FILE = new File(folderPath + "NumberOfPackages.txt");
     final static File PRIME_DAY_FILE = new File(folderPath + "PrimeDay.txt");
     final static double PRIME_DAY_DISCOUNT = .15;
+    private double profits = 0;
+    private double packagesShipped = 0;
+    private double numPackages = 0;
 
     /**
      * Main Method
-     *
+     * 
      * @param args list of command line arguements
      */
     public static void main(String[] args) {
-        //TODO
+    	//TODO
         Scanner s = new Scanner(System.in);
         String divider = "================";
-
-        //1) load data (vehicle, packages, profits, packages shipped and primeday) from files using DatabaseManager
-        ArrayList<Vehicle> vehicles;
-        ArrayList<Package> packages;
-        double profit;
-        int packagesShipped;
-        boolean primeDay;
+    	
+    	//1) load data (vehicle, packages, profits, packages shipped and primeday) from files using DatabaseManager
+        ArrayList<Vehicle> vehicles = DatabaseManager.loadVehicles(VEHICLE_FILE);
+        ArrayList<Package> packages = DatabaseManager.loadPackages(PACKAGE_FILE);
+        double profit = DatabaseManager.loadProfit(PROFIT_FILE);
+        int packagesShipped = DatabaseManager.loadPackagesShipped(N_PACKAGES_FILE);
+        boolean primeDay = DatabaseManager.loadPrimeDay(PRIME_DAY_FILE);
         String primeString;
 
         if (primeDay) {
@@ -39,9 +42,9 @@ public class Warehouse {
         } else {
             primeString = "Activate Prime Day";
         }
-
-        //2) Show menu and handle user inputs
-        while (true) {
+    	
+    	//2) Show menu and handle user inputs
+    	while (true) {
 
             System.out.println("========Options=========" +
                     "\n1) Add Package" +
@@ -54,10 +57,11 @@ public class Warehouse {
             String r = s.nextLine();
 
             switch(r) {
+                //Add package
                 case "1":
-
                     while (true) {
                         try {
+                            //Enter Package Statistics
                             System.out.println("Enter Package ID:");
                             String id = s.nextLine();
                             System.out.println("Enter Product Name:");
@@ -82,6 +86,7 @@ public class Warehouse {
                             s.next();
                             Package p = new Package(id, productName, weight, price,
                                     new ShippingAddress(buyerName, address, city, state, zipCode));
+                            packages.add(p);
                             System.out.println("\n\n" + p.shippingLabel());
                             break;
                         } catch (InputMismatchException e) {
@@ -89,8 +94,9 @@ public class Warehouse {
                         }
                     }
                     break;
-
+                //Add vehicle
                 case "2":
+                    //Add vehicle
                     System.out.println("Vehicle Options:" +
                             "\n1) Truck" +
                             "\n2) Drone" +
@@ -99,32 +105,35 @@ public class Warehouse {
                     while (true) {
                         try {
                             int intR = Integer.parseInt(r);
-
+                            //If the selection is out of range
                             if (intR < 0 || intR > 3) {
                                 System.out.println("Error: Option not available.");
                                 continue;
                             }
-
                             System.out.println("Enter License Plate No.:");
                             String licensePlate = s.nextLine();
                             s.next();
                             System.out.println("Enter Maximum Carry Weight:");
                             int carryWeight = s.nextInt();
                             s.next();
-
                             Vehicle v;
-
                             switch (intR) {
                                 case 1:
+                                    //Add truck
                                     v = new Truck(licensePlate, carryWeight);
                                     break;
                                 case 2:
+                                    //Add drone
                                     v = new Drone(licensePlate, carryWeight);
                                     break;
                                 case 3:
+                                    //Add cargo plane
                                     v = new CargoPlane(licensePlate, carryWeight);
                                     break;
+                                default:
+                                    throw new NumberFormatException();
                             }
+                            vehicles.add(v);
                         } catch (NumberFormatException e) {
                             System.out.println("Error: Option not available.");
                             continue;
@@ -132,12 +141,131 @@ public class Warehouse {
                         break;
                     }
                     break;
+                //Activate Prime day
                 case "3":
+                    primeDay = !primeDay;
+                    if (primeDay) {
+                        //Activate prime day
+                        for (Package temp: packages) {
+                            temp.setPrice(temp.getPrice() - (temp.getPrice() * PRIME_DAY_DISCOUNT));
+                        }
+                        primeString = "Deactivate Prime Day";
+                    } else {
+                        //Deactivate prime day
+                        for (Package temp: packages) {
+                            temp.setPrice(temp.getPrice() + (temp.getPrice() * PRIME_DAY_DISCOUNT));
+                        }
+                        primeString  = "Activate Prime Day";
+                    }
                     break;
+                //Send vehicle
                 case "4":
+                    Vehicle d = null;
+                    if (vehicles.size() == 0) {
+                        System.out.println("Error: No vehicles available.");
+                        break;
+                    }
+                    if (packages.size() == 0) {
+                        System.out.println("Error: No packages available.");
+                        break;
+                    }
+                    while (true) {
+                        System.out.println("Options:" +
+                                "\n1) Send Truck" +
+                                "\n2) Send Drone" +
+                                "\n3) Send Cargo Plane" +
+                                "\n4) Send First Available");
+                        r = s.nextLine();
+                        boolean hasVehicle;
+                        switch (r) {
+                            //Search for empty truck
+                            case "1":
+                                hasVehicle = false;
+                                for (Vehicle temp: vehicles) {
+                                    if (temp instanceof Truck && !temp.isFull()) {
+                                        d = temp;
+                                        hasVehicle = true;
+                                        break;
+                                    }
+                                }
+                                if (!hasVehicle) {
+                                    System.out.println("Error: No vehicles of selected type are available.");
+                                    continue;
+                                }
+                                break;
+                            //Search for empty drone
+                            case "2":
+                                hasVehicle = false;
+                                for (Vehicle temp: vehicles) {
+                                    if (temp instanceof Drone && !temp.isFull()) {
+                                        d = temp;
+                                        hasVehicle = true;
+                                        break;
+                                    }
+                                }
+                                if (!hasVehicle) {
+                                    System.out.println("Error: No vehicles of selected type are available.");
+                                    continue;
+                                }
+                                break;
+                            //Search for empty cargo plane
+                            case "3":
+                                hasVehicle = false;
+                                for (Vehicle temp: vehicles) {
+                                    if (temp instanceof CargoPlane && !temp.isFull()) {
+                                        d = temp;
+                                        hasVehicle = true;
+                                        break;
+                                    }
+                                }
+                                if (!hasVehicle) {
+                                    System.out.println("Error: No vehicles of selected type are available.");
+                                    continue;
+                                }
+                                break;
+                            //Search for first empty vehicles
+                            case "4":
+                                for (Vehicle temp: vehicles) {
+                                    if (!temp.isFull()) {
+                                        d = temp;
+                                        break;
+                                    }
+                                }
+                                break;
+                            default:
+                                System.out.println("Error: Option not available.");
+                                continue;
+                        }
+                        //Set the zip code
+                        while (true) {
+                            System.out.println("ZIP Code Options:" +
+                                    "\n1) Send to first ZIP Code" +
+                                    "\n2) Send to mode of ZIP Codes");
+                            r = s.nextLine();
+                            switch (r) {
+                                //Send to first zip code
+                                case "1":
+                                    d.setZipDest(packages.get(0).getDestination().getZipCode());
+                                    d.fill(packages);
+                                    profit = d.getProfit();
+
+                                    break;
+                                //Send to mode zip code
+                                case "2":
+                                    break;
+                                default:
+                                    System.out.println("Error Option not available.");
+                                    continue;
+                            }
+                            break;
+                        }
+                        break;
+                    }
                     break;
+                //Print statistics
                 case "5":
                     break;
+                //Exit Program
                 case "6":
                     return;
                 default:
@@ -145,11 +273,11 @@ public class Warehouse {
 
             }
         }
-
-
-        //3) save data (vehicle, packages, profits, packages shipped and primeday) to files (overwriting them) using DatabaseManager
-
-
+    	
+    	
+    	//3) save data (vehicle, packages, profits, packages shipped and primeday) to files (overwriting them) using DatabaseManager
+    	
+    
     }
 
 
